599647735c736dbdbc7d59f16df5bc583a6b4116
17moonup <17moonup@gmail.com>
2025-03-30 14:51:32 +0800

    add: film page

--- src/app/films/page.tsx
+++ src/app/films/page.tsx
@@ -1,11 +1,17 @@
 import styles from '../ui/film.module.css';
-
+import { Metadata } from 'next';
+import Navgation from '../components/Navgation';
+export const metadata: Metadata = {
+  title: 'Music',
+  };
 export default function Home(){
   return (
-
+    <div>
+  <Navgation />
 <div className={styles.scrollContainer}>
   <div className={styles.scrollItem}>第一页</div>
   <div className={styles.scrollItem}>第二页</div>
   <div className={styles.scrollItem}>第三页</div>
+</div>
 </div>
   )}
